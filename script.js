1b5421b54c036ee44ca13e2d28f96a7d7e400a28
= <=>
2026-08-28 15:47:25 +0530

    "First Remake"

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
-window.onload = function() {
+window.onload = function() 
+{
+    let k = 0;
+    for(X in projects) k++; //Count no of registered projects
     
     const hdr = document.getElementById('projects-holder')
-
-    let k = 0;
     
     for(X in projects) {
 
-        k++
-
         hdr.innerHTML +=
 
-        `
-        <div class="projectbox" id="WAD_LAB_${k}">
-                <p style="float: right; width: 40%;">
-                    Date of Creation: ${projects[X].DOC}
-                </p>
-
-                <h2>${projects[X].title}</h2>
+        `            
+            <div class="pcard" id="VU_WADLAB_${k}">
+                <i>
+                    ${projects[X].DOC} (Lab ${k--})
+                </i>
+                <h3 style="color: purple">
+                    ${projects[X].title}
+                </h3>
 
                 <hr>
 
-                <div style="float: right; width: 35%; border: 0px solid;">
-                    
-                    <br>
+                <p>
+                    <b>Description:- </b>
+                    <i>
+                        ${projects[X].desc}
+                    </i>
+                </p>
 
-                    <a href="${projects[X].path}" target="blank">
-                        <button class="open-button">Open Project</button>
-                    </a>
-
-                </div>
-
-                <div style="width: 60%; border-right: 1px solid; padding-right: 14px;">
-                    <p>
-                        <b>Description of Project:- </b>
-                        <i>
-                            ${projects[X].desc}
- 
-                        </i>
-                    </p>
-
-                    <br><br>
-
-                </div>             
+                <a href="${projects[X].path}" target="blank">
+                    <button class="open-button">Open Project</button>
+                </a>
 
             </div>
 
