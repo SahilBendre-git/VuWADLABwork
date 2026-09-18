6e4e9cd6e18a14161c7f250307f39dde9a2a672b
= <=>
2026-09-18 12:09:23 +0530

    "Fix1"

--- extra-projects.js
+++ extra-projects.js
@@ -4,6 +4,6 @@ const extra_projects = {
         title: "Club Sample Website",
         Date_Of_Issue: "17-09-2026",
         description: `Sample Club Website with Registration form`,
-        landing_page_path: "Wad Extras/Club Reg Form Professional/"
+        landing_page_path: "./Wad Extras/Club Reg Form Professional/"
     }
 }
